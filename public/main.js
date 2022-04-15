e36d054cf49458b42bfba6346476df0aff61bb69
Christine Mendoza <christine.mendoza@unc.edu>
2022-04-15 00:29:43 -0400

    implement div single

--- public/main.js
+++ public/main.js
@@ -20,15 +20,22 @@ function flipCoin() {
       .then(function (result) {
         console.log(result);
         document.getElementById("result").innerHTML = result.flip;
-        document.getElementById("quarter").setAttribute("src", result.flip + ".jpg");
-        coin.disabled = true
+        document.getElementById("quarter").setAttribute("src", "assets/img/" + result.flip + ".png");
       })
-    //				let flip = "FLIPPED"
-    //				document.getElementById("coin").innerHTML = flip;
-    //				console.log("Coin has been flipped. Result: "+ flip)
 }
 
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
+function flipCoins(number) {
+    fetch('http://localhost:5000/app/flip/')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (result) {
+        console.log(result);
+        document.getElementById("result").innerHTML = result.flip;
+        document.getElementById("quarter").setAttribute("src", "assets/img/" + result.flip + ".png");
+      })    
+}
 
 // Guess a flip by clicking either heads or tails button
