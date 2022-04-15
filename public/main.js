0a5ffa4c05983544652aa5fa2876d7c68cfc683f
Christine Mendoza <christine.mendoza@unc.edu>
2022-04-15 15:27:59 -0400

    show summary for multiple flips

--- public/main.js
+++ public/main.js
@@ -3,7 +3,7 @@ function focusDiv(divname) {
     // set active element(s) to be hidden
     var activeDivsCollection = document.getElementsByClassName("active");
     var activeDivsArr = Array.from(activeDivsCollection)
-    activeDivsArr.forEach(function(currentdiv) {
+    activeDivsArr.forEach(function (currentdiv) {
         currentdiv.setAttribute("class", "hidden");
     })
 
@@ -14,28 +14,43 @@ function focusDiv(divname) {
 // Flip one coin and show coin image to match result when button clicked
 function flipCoin() {
     fetch('http://localhost:5000/app/flip/')
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (result) {
-        console.log(result);
-        document.getElementById("result").innerHTML = result.flip;
-        document.getElementById("quarter").setAttribute("src", "assets/img/" + result.flip + ".png");
-      })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (result) {
+            console.log(result);
+            document.getElementById("flipResult").innerHTML = result.flip;
+            document.getElementById("quarter").setAttribute("src", "assets/img/" + result.flip + ".png");
+        })
 }
 
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
-function flipCoins(number) {
-    fetch('http://localhost:5000/app/flip/')
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (result) {
-        console.log(result);
-        document.getElementById("result").innerHTML = result.flip;
-        document.getElementById("quarter").setAttribute("src", "assets/img/" + result.flip + ".png");
-      })    
+function flipCoins() {
+    // retrieve number of coins to flip
+    numberCoins = document.getElementById("numberCoins").value;
+
+    fetch('http://localhost:5000/app/flips/coins', {
+        body: JSON.stringify({
+            "number": numberCoins
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+        method: "post"
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (result) {
+            console.log(result);
+            document.getElementById("summaryHeads").innerHTML = result.summary.heads;
+            document.getElementById("summaryTails").innerHTML = result.summary.tails;
+            // document.getElementById("quarter").setAttribute("src", "assets/img/" + result.flip + ".png");
+
+            // Show results
+            document.getElementById("resultTables").setAttribute("class", "active");
+        })
 }
 
 // Guess a flip by clicking either heads or tails button
